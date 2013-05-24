c1887234b6a31e09c914f6779fedc8f14bda181f
huyu <hyuuhit@gmail.com>
2013-05-25 02:45:59 +0800

    update connector

--- connector.c
+++ connector.c
@@ -6,7 +6,6 @@
 #include <sys/time.h>
 
 #include "connector.h"
-#include "ae.h"
 #include "mem.h"
 #include "net.h"
 #include "logging.h"
@@ -14,7 +13,9 @@
 #define MAX_RECV_BUF_LEN    0x100000
 #define MAX_SEND_Q_COUNT    128
 
-#define MAX_CONNECTOR_SIZE  AE_SETSIZE
+
+#define CONNECTOR_SLOT_NUM  8192     // 2^13
+#define get_connector_index(fd)     ((fd) % CONNECTOR_SLOT_NUM) // fd & (CONNECTOR_SLOT_NUM - 1)
 
 typedef struct send_q_t {
     void            *send_buf;  // 待发送数据
@@ -24,27 +25,68 @@ typedef struct send_q_t {
 } send_q_t;
 
 typedef struct connector_t {
+    int             fd;
     struct timeval  created_at;
     struct timeval  updated_at;
-    char        closed_flag;                // 被动关闭标记，不接收不发送，等待处理计数
-    char        closing_flag;               // 主动关闭标记，不接收只发送，发送后清理
-    char        ip[INET_ADDRSTRLEN];        // 客户端 IP
-    int         port;                       // 客户端 PORT
-    char        recv_buf[MAX_RECV_BUF_LEN]; // 数据接收缓冲区
-    int         recv_len;                   // 已接收字节数
-    send_q_t    send_q_head;                // 待发送数据包队列
-    int         send_q_count;               // 待发送数据包个数
-    int         processing_count;           // 处理中的数据包计数，非0时不清理
+    char            closed_flag;                // 被动关闭标记，不接收不发送，等待处理计数
+    char            closing_flag;               // 主动关闭标记，不接收只发送，发送后清理
+    char            ip[INET_ADDRSTRLEN];        // 客户端 IP
+    int             port;                       // 客户端 PORT
+    char            recv_buf[MAX_RECV_BUF_LEN]; // 数据接收缓冲区
+    int             recv_len;                   // 已接收字节数
+    send_q_t       *send_q_head;                // 待发送数据包队列
+    int             send_q_count;               // 待发送数据包个数
+    int             processing_count;           // 处理中的数据包计数，非0时不清理
+
+    struct connector_t *next;
 } connector_t;
 
-connector_t *connector_ptr_array[MAX_CONNECTOR_SIZE] = {NULL};
+connector_t *connector_ptr_slot[CONNECTOR_SLOT_NUM] = {NULL};
 
-// 设置连接主动关闭选项，不接收只发送
-int connector_set_closing(int fd) {
-    connector_t *con = connector_ptr_array[fd];
+
+// 获取结构体指针
+static connector_t *connector_get_ptr(int fd) {
+    int index = get_connector_index(fd);
+    connector_t *con = connector_ptr_slot[index];
+    while(con != NULL && con->fd != fd)
+        con = con->next;
+    if(con == NULL)
+        logging_fatal("NULL connector for fd %d", fd);
+    return con;
+}
+
+// 清除该连接所有数据，不考虑各种计数，内部校验合法后调用
+static void connector_destroy(int fd) {
+    int index = get_connector_index(fd);
+    connector_t **curr = &connector_ptr_slot[index];
+    while(*curr) {
+        connector_t *con = *curr;
+        if(con->fd == fd) {
+            *curr = con->next;
+            close(fd);
+            send_q_t **q_curr = &con->send_q_head;
+            while(*q_curr) {
+                send_q_t *q_ptr = *q_curr;
+                *q_curr = q_ptr->next;
+                mem_free(q_ptr->send_buf);
+                mem_free(q_ptr);
+            }
+            logging_trace("%s:%d %d destroy", con->ip, con->port, fd);
+            mem_free(con);
+            // 跳出
+            break;
+        }
+        else {
+            curr = &con->next;
+        }
+    }
+}
+
+// 设置连接主动关闭选项，发送链完结后可以清空该连接
+void connector_set_closing(int fd) {
+    connector_t *con = connector_get_ptr(fd);
     con->closing_flag = 1;
     logging_trace("%s:%d %d set_closing", con->ip, con->port, fd);
-    return 0;
 }
 
 // 初始化连接数据
@@ -52,48 +94,35 @@ int connector_init(int fd, const char *ip, int port) {
     connector_t *con = mem_malloc(sizeof(connector_t));
     gettimeofday(&(con->created_at), NULL);
     con->updated_at = con->created_at;
+    con->fd = fd;
     con->closed_flag = 0;
     con->closing_flag = 0;
     memcpy(con->ip, ip, strlen(ip));
     con->port = port;
     con->recv_len = 0;
-    con->send_q_head.next = NULL;
+    con->send_q_head = NULL;
     con->send_q_count = 0;
     con->processing_count = 0;
 
-    connector_ptr_array[fd] = con;
+    int index = get_connector_index(fd);
+    con->next = connector_ptr_slot[index];
+    connector_ptr_slot[index] = con;
     logging_trace("%s:%d %d init", ip, port, fd);
     return 0;
 }
 
-// 清除该连接所有数据，不考虑各种计数，内部校验合法后调用
-static int connector_destroy(int fd) {
-    connector_t *con = connector_ptr_array[fd];
-    close(fd);
-    connector_ptr_array[fd] = NULL;
-    send_q_t *q_ptr = con->send_q_head.next;
-    while(q_ptr != NULL) {
-        q_ptr = q_ptr->next;
-        mem_free(q_ptr);
-    }
-    logging_trace("%s:%d %d destroy", con->ip, con->port, fd);
-    mem_free(con);
-    return 0;
-}
-
-int connector_get_info(int fd, char **ip, int *port, void **buf, int *len) {
-    connector_t *con = connector_ptr_array[fd];
+void connector_get_info(int fd, char **ip, int *port, void **buf, int *len) {
+    connector_t *con = connector_get_ptr(fd);
     *ip = con->ip;
     *port = con->port;
     *buf = con->recv_buf;
     *len = con->recv_len;
     logging_trace("%s:%d %d get_info recv_len %d", con->ip, con->port, fd, con->recv_len);
-    return 0;
 }
 
 // 返回-1，未准备好可以处理的数据
 int connector_pop_packet(int fd, void **data, int len) {
-    connector_t *con = connector_ptr_array[fd];
+    connector_t *con = connector_get_ptr(fd);
     if(con->recv_len < len) {
         logging_trace("%s:%d %d pop_packet not ready", con->ip, con->port, fd);
         return -1;
@@ -114,7 +143,7 @@ int connector_pop_packet(int fd, void **data, int len) {
 // 返回-1，发送过慢异常，清空事件
 // 返回 1，发送链刚刚建立，需要建立写事件
 int connector_push_packet(int fd, void *data, int len) {
-    connector_t *con = connector_ptr_array[fd];
+    connector_t *con = connector_get_ptr(fd);
     // 连接引用计数
     con->processing_count --;
     logging_trace("%s:%d %d push_packet %d bytes, processing_count %d", con->ip, con->port, fd, con->processing_count);
@@ -133,20 +162,21 @@ int connector_push_packet(int fd, void *data, int len) {
     }
 
     logging_trace("%s:%d %d send_q_count %d", con->ip, con->port, fd, con->send_q_count);
-    send_q_t *pre_ptr = &(con->send_q_head);
-    while(pre_ptr->next != NULL) pre_ptr = pre_ptr->next;
     send_q_t *q_entry = (send_q_t *)mem_malloc(sizeof(send_q_t));
     q_entry->send_buf = data;
     q_entry->buf_len = len;
     q_entry->send_len = 0;
-    q_entry->next = pre_ptr->next;
-    pre_ptr->next = q_entry;
+    q_entry->next = NULL;
+
+    send_q_t **curr = &con->send_q_head;
+    while(*curr) *curr = (*curr)->next;
+    *curr = q_entry;
     return con->send_q_count;
 }
 
 // -1，连接超时异常，需要清空事件
 int connector_check_one(int fd, struct timeval *tv, int idle_timeout_ms) {
-    connector_t *con = connector_ptr_array[fd];
+    connector_t *con = connector_get_ptr(fd);
     if(con == NULL)
         return 0;
     if(con->closed_flag && con->processing_count == 0) {
@@ -179,7 +209,7 @@ int connector_check_one(int fd, struct timeval *tv, int idle_timeout_ms) {
 
 // -1，连接状态异常，需要清空事件
 int connector_recv(int fd) {
-    connector_t *con = connector_ptr_array[fd];
+    connector_t *con = connector_get_ptr(fd);
     // 缓冲区满继续期望读，返回异常
     if(con->recv_len == MAX_RECV_BUF_LEN) {
         logging_trace("%s:%d %d recv_buf overflow", con->ip, con->port, fd);
@@ -205,13 +235,15 @@ int connector_recv(int fd) {
 // -1，连接状态异常，需要清空事件
 //  0, 发送链空，需要清除写事件
 int connector_send(int fd) {
-    connector_t *con = connector_ptr_array[fd];
+    connector_t *con = connector_get_ptr(fd);
     while(1) {
-        send_q_t *pre_ptr = &(con->send_q_head);
-        send_q_t *s_ptr = pre_ptr->next;
-        if(s_ptr == NULL)
+        //send_q_t *pre_ptr = &(con->send_q_head);
+        //send_q_t *s_ptr = pre_ptr->next;
+        send_q_t **curr = &con->send_q_head;
+        send_q_t *q_entry = *curr;
+        if(q_entry == NULL)
             break;
-        int num = net_write_n(fd, (char *)(s_ptr->send_buf) + s_ptr->send_len, s_ptr->buf_len - s_ptr->send_len);
+        int num = net_write_n(fd, (char *)(q_entry->send_buf) + q_entry->send_len, q_entry->buf_len - q_entry->send_len);
         logging_trace("%s:%d %d write %d bytes", con->ip, con->port, fd, num);
         if(num == 0) {
             break;
@@ -223,12 +255,12 @@ int connector_send(int fd) {
             return -1;
         }
         else {
-            s_ptr->send_len += num;
-            logging_trace("%s:%d %d send_buf_len %d, send_len %d", con->ip, con->port, fd, s_ptr->buf_len, s_ptr->send_len);
-            if(s_ptr->send_len == s_ptr->buf_len) {
-                pre_ptr->next = s_ptr->next;
-                mem_free(s_ptr->send_buf);
-                mem_free(s_ptr);
+            (*curr)->send_len += num;
+            logging_trace("%s:%d %d send_buf_len %d, send_len %d", con->ip, con->port, fd, q_entry->buf_len, q_entry->send_len);
+            if(q_entry->send_len == q_entry->buf_len) {
+                *curr = (*curr)->next;
+                mem_free(q_entry->send_buf);
+                mem_free(q_entry);
                 // 发送队列计数
                 con->send_q_count --;
                 logging_trace("%s:%d %d send_q_count %d", con->ip, con->port, fd, con->send_q_count);
@@ -236,4 +268,12 @@ int connector_send(int fd) {
         }
     }
     return con->send_q_count;
+}
+
+void connector_test() {
+    printf("%d\n", get_connector_index(19));
+    printf("%d\n", get_connector_index(0));
+    printf("%d\n", get_connector_index(8192));
+    printf("%d\n", get_connector_index(8193));
+    printf("%d\n", get_connector_index(8195));
 }
--- connector.h
+++ connector.h
@@ -2,14 +2,14 @@
 #define _CONNECTOR_H_
 
 
-int connector_set_closing(int fd);
-int connector_init(int fd, const char *ip, int port);
-int connector_recv(int fd);
-int connector_send(int fd);
-int connector_check_all(int idle_timeout);
-int connector_get_info(int fd, char **ip, int *port, void **buf, int *len);
-int connector_pop_packet(int fd, void **data, int len);
-int connector_push_packet(int fd, void *data, int len);
-int connector_check_one(int fd, struct timeval *tv, int idle_timeout_ms);
+void    connector_set_closing(int fd);
+int     connector_init(int fd, const char *ip, int port);
+int     connector_recv(int fd);
+int     connector_send(int fd);
+int     connector_check_all(int idle_timeout);
+void    connector_get_info(int fd, char **ip, int *port, void **buf, int *len);
+int     connector_pop_packet(int fd, void **data, int len);
+int     connector_push_packet(int fd, void *data, int len);
+int     connector_check_one(int fd, struct timeval *tv, int idle_timeout_ms);
 
 #endif
